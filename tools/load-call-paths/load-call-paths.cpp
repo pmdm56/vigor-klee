0607ddfca874b57e314d15816b17bac23d4c6224
Francisco Pereira <fchamicapereira@gmail.com>
2020-05-20 22:14:21 +0100

    cleanup of unused code

--- tools/load-call-paths/load-call-paths.cpp
+++ tools/load-call-paths/load-call-paths.cpp
@@ -20,48 +20,13 @@
 #include <klee/Constraints.h>
 #include <klee/Solver.h>
 #include <vector>
-#include <algorithm>
-#include <iomanip>
-#include <algorithm>
-
-// term colors
-// src: https://stackoverflow.com/questions/9158150/colored-output-in-c/9158263
-#define RESET "\033[0m"
-#define BLACK "\033[30m"              /* Black */
-#define RED "\033[31m"                /* Red */
-#define GREEN "\033[32m"              /* Green */
-#define YELLOW "\033[33m"             /* Yellow */
-#define BLUE "\033[34m"               /* Blue */
-#define MAGENTA "\033[35m"            /* Magenta */
-#define CYAN "\033[36m"               /* Cyan */
-#define WHITE "\033[37m"              /* White */
-#define BOLDBLACK "\033[1m\033[30m"   /* Bold Black */
-#define BOLDRED "\033[1m\033[31m"     /* Bold Red */
-#define BOLDGREEN "\033[1m\033[32m"   /* Bold Green */
-#define BOLDYELLOW "\033[1m\033[33m"  /* Bold Yellow */
-#define BOLDBLUE "\033[1m\033[34m"    /* Bold Blue */
-#define BOLDMAGENTA "\033[1m\033[35m" /* Bold Magenta */
-#define BOLDCYAN "\033[1m\033[36m"    /* Bold Cyan */
-#define BOLDWHITE "\033[1m\033[37m"   /* Bold White */
 
 #define DEBUG
-
-#define UINT_16_SWAP_ENDIANNESS(p) ((((p) & 0xff) << 8) | ((p) >> 8 & 0xff))
 
 namespace {
 llvm::cl::list<std::string> InputCallPathFiles(llvm::cl::desc("<call paths>"),
                                                llvm::cl::Positional,
                                                llvm::cl::OneOrMore);
-}
-
-std::string expr_to_string(klee::expr::ExprHandle expr) {
-  std::string expr_str;
-  if (expr.isNull())
-    return expr_str;
-  llvm::raw_string_ostream os(expr_str);
-  expr->print(os);
-  os.str();
-  return expr_str;
 }
 
 typedef struct {
